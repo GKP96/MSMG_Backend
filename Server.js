6100bd3e3ccfe41ae90365a14dd580bae1948ae5
gkp96 <gautam2002pandeyjee@gmail.com>
2024-06-07 14:06:57 +0530

    auth part is completed

---
+++ Server.js
@@ -0,0 +1,43 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import UserRouter from "./routes/UserRouter.js";
+import PropertyRouter from "./routes/PropertyRouter.js";
+import AuthRouter from "./routes/AuthRouter.js";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+// routes
+app.use("/users", UserRouter);
+app.use("/properties", PropertyRouter);
+app.use("/auth", AuthRouter);
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    status: err.statusCode || 500,
+    success: false,
+  });
+});
+// const url =
+//   "mongodb+srv://Gautama:Gaunik%401234@cluster1.txuuzz9.mongodb.net/MSMG?retryWrites=true&w=majority&appName=Cluster1";
+const url = "mongodb://localhost:27017/MSMG"
+mongoose.connect(url);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error: "));
+db.once("open", function () {
+  console.log("Database Connected successfully");
+});
+
+const port = process.env.PORT || 5050;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
